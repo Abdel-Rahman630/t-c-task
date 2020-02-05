c783fa44d5570fb0751a7196a6e2f5a2869784b8
Abdel-Rahman630 <boodyronaldo@gmail.com>
2020-02-05 21:57:13 +0200

    Search Section

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,60 @@
 $(document).ready(function () {
 
+    /********************************* SideBar ************************************/
+
     // Toggle Slider
     $(".sidebar .icon").click(function () {
+
+        $(".sidebar").removeClass("left")
+
+    })
+
+    $("nav .bar").click(function () {
 
         $(".sidebar").addClass("left")
 
     })
 
+
+
+    // Class Active
     $(".sidebar ul li").click( function () {
 
         $(this).addClass("active").siblings().removeClass("active")
+        
         
     })
 
     // Slide toggle
     $(".sidebar ul li").click(function () {
 
-        $(this).children("ul").slideToggle(500)
-        
+        $(this).children("ul").slideToggle(500),
+        $(this).siblings("li").children("ul").hide(500)
+
+    })
+    
+    /************************ Search Section ******************************** */
+    $("nav i.fa-search").click(function () {
+
+        $(".input-search").fadeIn(300)
 
     })
 
-    
+    $(".input-search .icon2").click(function () {
+
+        $(this).parent(".input-search").fadeOut(300)
+
+    })
+
+    $(".input-search .parent input").focus(function () {
+
+        $(this).attr('placeholder', "")
+
+    })
+
+    $(".input-search .parent input").blur(function () {
+
+        $(this).attr('placeholder', "Search")
+
+    })
 })
